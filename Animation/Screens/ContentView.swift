ec8e35268dd9a3c8cdbb198c1715ddfce79a3567
BekDevIOS <umekhmonboev@gmail.com>
2023-10-25 19:53:16 +0900

    Initial Commit

---
+++ Animation/Screens/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Animation
+//
+//  Created by Utkirbek Mekhmonboev on 10/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+   
+    var body: some View {
+        Text("Home")
+          
+    }
+}
+
+#Preview {
+    ContentView()
+}
