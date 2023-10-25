88c0dc1e162dbbbb8de0de469986fb1d07a86b83
BekDevIOS <umekhmonboev@gmail.com>
2023-10-25 21:47:51 +0900

    update!

---
+++ Animation/Screens/Task1Screen.swift
@@ -0,0 +1,18 @@
+//
+//  Task1Screen.swift
+//  Animation
+//
+//  Created by Utkirbek Mekhmonboev on 10/25/23.
+//
+
+import SwiftUI
+
+struct Task1Screen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Task1Screen()
+}
